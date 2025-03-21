c44d0db8885b14fd15b39e34d9c3550aaaf28a44
raffico <raffael.vieira@escola.pr.gov.br>
2025-03-21 10:58:43 -0300

    test 4

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 let selecaoPersonagem = document.querySelectorAll(".selecao-personagem")
 console.log(selecaoPersonagem)
-selecaoPersonagem.forEach(element => {
-    console.log(element);
-});
+for (let element of selecaoPersonagem) {
+    console.log(element)
+}

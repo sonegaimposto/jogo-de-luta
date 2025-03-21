5b4b3f147eb2d62db1a77b68a0da96f8c765cdf7
raffico <raffael.vieira@escola.pr.gov.br>
2025-03-21 11:10:11 -0300

    teste 5

--- menu.js
+++ menu.js
@@ -1,5 +1,14 @@
 let selecaoPersonagem = document.querySelectorAll(".selecao-personagem")
-console.log(selecaoPersonagem)
+
+function addPersonagem(nome,img) {
+    let personagem = document.createElement("div")
+    personagem.classList.add("personagem")
+    personagem.innerHTML = `
+        <button onclick="escolherPersonagem(${nome})">
+        <img class="personagem-imagem" src="assets/personagens/menu/${img}.png">
+    `;
+    return personagem;
+}
 for (let element of selecaoPersonagem) {
-    console.log(element)
+    element.appendChild(addPersonagem("Goku","goku"));
 }
